f9976adf6de691709a24d1723f39176c9ef42359
Slindile <slindilenkabs23@gmail.com>
2026-02-04 22:02:36 +0200

    my first title and description of the app

--- HabitTracker/app.py
+++ HabitTracker/app.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import streamlit as st
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+title = st.title("Habit Tracker")
+description = st.write("This dashboard shows my weekly habit progress")
+    
+   
