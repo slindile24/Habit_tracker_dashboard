4632b892be399247dc6a0d067ec3a56ce9c38c31
Slindile <slindilenkabs23@gmail.com>
2026-02-05 20:25:49 +0200

    added other columns too , made sure each column has 7 numbers each

--- HabitTracker/app.py
+++ HabitTracker/app.py
@@ -12,3 +12,8 @@ description = st.write("This dashboard shows my weekly habit progress")
 
 days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"] 
    
+# now creating a list for all the other columns 
+study_hours = [1,2,3,4,5,6,7]
+litres_of_water = [1,2,3,4,5,6,7]
+exercise_mins = [10,15,30,35,45,50,60]
+
