8f8e838dfd4bf358e27232c29d5e9fa1929178ac
Slindile <slindilenkabs23@gmail.com>
2026-02-05 20:18:42 +0200

    starting off by creating the list of days for my small dataset

--- HabitTracker/app.py
+++ HabitTracker/app.py
@@ -4,5 +4,11 @@ import matplotlib.pyplot as plt
 
 title = st.title("Habit Tracker")
 description = st.write("This dashboard shows my weekly habit progress")
-    
+
+
+#Now I am going to create a small dataset 
+
+#It will include Days, Study hours, Water and Exercise
+
+days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"] 
    
